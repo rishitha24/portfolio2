6a981fc0cee2cf7d5cc7add8bc99360c93131522
Rishitha <44165491+rishitha24@users.noreply.github.com>
2020-10-22 19:44:34 +0530

    Add files via upload

---
+++ vanta.js canvas/app.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 0.4,
+  scaleMobile: 0.1,
+  color: 0x158b30,
+  backgroundColor: 0x30207,
+  points: 15.00,
+  maxDistance: 17.00,
+  spacing: 13.00
+})
